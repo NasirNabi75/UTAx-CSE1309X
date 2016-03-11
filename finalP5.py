5c05531550da3e12f64c7c2b61c339d87eb30d0d
aliyyousuff <aliyyousuf@gmail.com>
2016-03-11 14:33:21 +0600

    final

--- finalP5.py
+++ finalP5.py
@@ -1,4 +1,12 @@
-D = {'Accurate': ['exact', 'precise'], 'exact': ['precise'], 'astute': ['Smart', 'clever'], 'smart': ['clever', 'bright', 'talented']}
-D2= {}
-for k,v in D.items():
-    
+__author__ = 'Mohammad Yousuf Ali,aliyyousuf@gmail.com'
+
+
+def MY_2D_LIST(n):
+    r1, r2 = [1], [1, 1]
+    degree = 1
+    R = []
+    while degree <= n:
+        R += [r1]
+        r1, r2 = r2, [1] + [sum(pair) for pair in zip(r2, r2[1:]) ] + [1]
+        degree += 1
+    return R
